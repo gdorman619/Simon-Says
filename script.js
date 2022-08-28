55fbbbcb65475ec6548572084fd8a45c2c219b07
garydorman <garydorman619@gmail.com>
2022-08-28 14:28:25 -0400

    Added logic for cpu sequence.

--- script.js
+++ script.js
@@ -1,55 +1,87 @@
-const boxes = document.querySelectorAll(".box")
-const startButton = document.querySelector("#start-button")
-let audioRed = new Audio("audio/bootsy__saturn-loop.wav")
-let audioGreen = new Audio("audio/jungle__water-drop.wav")
-let audioBlue = new Audio("audio/madjad__indonesian-thum-strike.wav")
-let audioYellow = new Audio("audio/neatonk__piano-med.wav")
+const boxes = document.querySelectorAll(".box");
+const startButton = document.querySelector("#start-button");
+let audioRed = new Audio("audio/bootsy__saturn-loop.wav");
+let audioGreen = new Audio("audio/jungle__water-drop.wav");
+let audioBlue = new Audio("audio/madjad__indonesian-thum-strike.wav");
+let audioYellow = new Audio("audio/neatonk__piano-med.wav");
 
 
+let cpuSequence = [];
+let userSequence = [];
+let round = 0;
 
-let numRounds = 5;
-let userScore = 0
-let cpuBoxesSelectedList = []
-
-function selectBox() {
-     // Returns a random integer from 0 to 3:
-     let randomNum = Math.floor(Math.random() * 4);
+function selectBox(boxNum) {
     
-     console.log(boxes[randomNum].id);
-     boxes[randomNum].classList.add("selected")
-     boxes[randomNum].addEventListener("transitionend", function (e) {
-        if (boxes[randomNum].id === 'box-1'){
+     boxes[boxNum].classList.add("selected")
+
+     boxes[boxNum].addEventListener("transitionend", function (e) {
+        if (boxes[boxNum].id === 'box-1'){
             audioRed.play();
         }
-        if (boxes[randomNum].id === 'box-2'){
+        if (boxes[boxNum].id === 'box-2'){
             audioGreen.play();
         }
-        if (boxes[randomNum].id === 'box-3'){
+        if (boxes[boxNum].id === 'box-3'){
             audioBlue.play();
         }
-        if (boxes[randomNum].id === 'box-4'){
+        if (boxes[boxNum].id === 'box-4'){
             audioYellow.play();
         }
         this.classList.remove("selected")
      });
-    cpuBoxesSelectedList.push(boxes[randomNum].id)
   };
 
-  function selectBoxLoop(loopCountParam){
-    loopCount = loopCountParam
-    var intervalId = window.setInterval(function(){
-        selectBox();
-        loopCount--;
-        if (loopCount === 0){
-            console.log(cpuBoxesSelectedList)
-            clearInterval(intervalId)
-            loopCount = loopCountParam
+  function cpuSequenceLoop(){
+
+    if (cpuSequence.length === 0){
+        addCpuSequenceNumber();
+    }
+
+    let cpuSequenceCounter = 0;
+    
+    let intervalId = window.setInterval(function(){
+        selectBox(cpuSequence[cpuSequenceCounter]);
+        if (cpuSequenceCounter === cpuSequence.length - 1){
+            cpuSequenceCounter = 0;
+            clearInterval(intervalId);
+        }
+        else{
+            cpuSequenceCounter += 1;
         }
       }, 1200);
+  };
+
+  function addCpuSequenceNumber(){
+    let randomNum = Math.floor(Math.random() * 4);
+    console.log(randomNum);
+    cpuSequence.push(randomNum);
   }
 
+  boxes[0].addEventListener("click", function (e){
+    console.log('red');
+    selectBox(0);
+  });
+
+  boxes[1].addEventListener("click", function (e){
+    console.log('green');
+    selectBox(1);
+  });
+
+  boxes[2].addEventListener("click", function (e){
+    console.log('blue');
+    selectBox(2);
+  });
+
+  boxes[3].addEventListener("click", function (e){
+    console.log('yellow');
+    selectBox(3);
+  });
+
+
   startButton.addEventListener("click", function (e){
-    selectBoxLoop(numRounds);
+    round = 1;
+    cpuSequence = []
+    cpuSequenceLoop();
   });
   
   
